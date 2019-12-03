d8b0fbace4b4c45727a1f29a63cbbe89af8c3955
EC2 Default User <ec2-user@ip-172-31-93-62.ec2.internal>
2019-12-03 01:54:24 +0000

    Play single round

--- rockpaper.js
+++ rockpaper.js
@@ -1,7 +1,19 @@
 let choices = ["Rock","Paper","Scissors"]
 
+const rl = require('readline-sync');
+
 function computerPlay() {
-  return choices[randNum(2)]
+  return choices[randNum(2)];
+}
+
+function personPlay() {
+
+  let choice = rl.question('What do you choose? (Rock, Paper, or Scissors) ').trim().toUpperCase()
+    
+  if (choice == "ROCK" || choice == "PAPER" || choice == "SCISSORS") {
+    return choice ;
+  }
+  personPlay();
 }
 
 function randNum(maxNum) { // Returns a random integer between 0 and maxNum inclusive
@@ -12,27 +24,36 @@ function compareChoices(playerOneChoice, playerTwoChoice) { //Returns the winnin
   
   switch (playerOneChoice) { 
   
-    case "Rock":
-      if (playerTwoChoice == "Paper") {
-        return 2
+    case "ROCK":
+      if (playerTwoChoice == "PAPER") {
+        return 2;
       } else {
-        return 1
+        return 1;
       }
       
-    case "Paper":
-      if (playerTwoChoice == "Scissors") {
-        return 2
+    case "PAPER":
+      if (playerTwoChoice == "SCISSORS") {
+        return 2;
       } else {
-        return 1
+        return 1;
       }
       
-    case "Scissors":
-      if (playerTwoChoice == "Rock") {
-        return 2
+    case "SCISSORS":
+      if (playerTwoChoice == "ROCK") {
+        return 2;
       } else {
-        return 1
+        return 1;
       }
     }
 }
 
+function declareWinner(winner) {
+  console.log(`Player ${winner} wins!`);
+}
 
+function playRound() {
+  let winningPlayer = compareChoices(personPlay(), computerPlay())
+  declareWinner(winningPlayer)
+}
+
+playRound()
