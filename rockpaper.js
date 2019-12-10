c44d6cc9c677d3e144ec97f8bcd6c5b7a6a64608
Peter Brandon <peterkbrandon@gmail.com>
2019-12-09 20:39:18 -0500

    refactor to allow for front end gameplay

--- rockpaper.js
+++ rockpaper.js
@@ -1,47 +1,73 @@
 let choices = ["ROCK","PAPER","SCISSORS"];
+let p1Wins = 0;
+let p2Wins = 0;
+let roundsToWin = 5;
 
-const rl = require('readline-sync');
+//Math.ceil(rounds/2);
+
+let selectors = {
+  rockButton : document.getElementById("RockButton"),
+  paperButton : document.getElementById("PaperButton"),
+  scissorsButton : document.getElementById("ScissorsButton"),
+  playerWinCount : document.getElementById("PlayerWinCount"),
+  computerWinCount : document.getElementById("ComputerWinCount"),
+  messages : document.getElementById("Messages"),
+  choiceMessage : document.getElementById("ChoiceMessage"),
+  winMessage : document.getElementById("WinMessage")
+}
+
+selectors.rockButton.addEventListener("click", (e) => {
+  playRound("ROCK");
+});
+
+selectors.paperButton.addEventListener("click", (e) => {
+  playRound("PAPER");
+});
+
+selectors.scissorsButton.addEventListener("click", (e) => {
+  playRound("SCISSORS");
+});
+
+
+function setPlayerScore(score) {
+   selectors.playerWinCount.innerText = `Player Wins: ${score}`;
+}
+
+function setComputerScore(score) {
+   selectors.computerWinCount.innerText = `Computer Wins: ${score}`;
+}
 
 function computerPlay() {
   return choices[randNum(2)];
 }
 
-function personPlay() {
-
-  let choice = rl.question('What do you choose? (Rock, Paper, or Scissors) ').trim().toUpperCase();
-    
-  if (choice == "ROCK" || choice == "PAPER" || choice == "SCISSORS") {
-    return choice;
-  }
-  personPlay();
-}
 
 function randNum(maxNum) { // Returns a random integer between 0 and maxNum inclusive
-  return Math.floor(Math.random()*(maxNum+1)); 
+  return Math.floor(Math.random()*(maxNum+1));
 }
 
 function compareChoices(playerOneChoice, playerTwoChoice) { //Returns the winning player as represented by a number
-  
+
   if (playerOneChoice == playerTwoChoice) {
     return 3;
   }
-  
-  switch (playerOneChoice) { 
-  
+
+  switch (playerOneChoice) {
+
     case "ROCK":
       if (playerTwoChoice == "PAPER") {
         return 2;
       } else {
         return 1;
       }
-      
+
     case "PAPER":
       if (playerTwoChoice == "SCISSORS") {
         return 2;
       } else {
         return 1;
       }
-      
+
     case "SCISSORS":
       if (playerTwoChoice == "ROCK") {
         return 2;
@@ -52,54 +78,52 @@ function compareChoices(playerOneChoice, playerTwoChoice) { //Returns the winnin
 }
 
 function declareWinner(winner, context) {
-  console.log(`Player ${winner} wins the ${context}!`);
+  selectors.winMessage.innerText = `Player ${winner} wins the ${context}!`;
+  if (context == "game") {
+    p1Wins = 0;
+    p2Wins = 0;
+  }
+
+  setPlayerScore(p1Wins);
+  setComputerScore(p2Wins);
 }
 
 function declareTie() {
-  console.log(`It's a tie!`);
+  selectors.winMessage.innerText = `It's a tie!`;
 }
 
-function playRound() {
-  let personChoice = personPlay();
+function playRound(personChoice) {
   let computerChoice = computerPlay();
-  let winningPlayer = compareChoices(personChoice, computerChoice);
-  console.log(`Player chose ${personChoice} and computer chose ${computerChoice}`);
-
-  return winningPlayer;
+  selectors.choiceMessage.innerText = `Player chose ${personChoice} and computer chose ${computerChoice}`;
+  keepScore(compareChoices(personChoice, computerChoice));
 }
 
-function playGame(rounds) {
-  let p1Wins = 0;
-  let p2Wins = 0;
-  let roundsToWin = Math.ceil(rounds/2);
-  for (let i = 0; i < rounds; i++) {
+function keepScore(winner) {
 
-    switch (playRound()) {
+    switch (winner) {
       case 1:
-        declareWinner(1, 'round');
         p1Wins++;
+        declareWinner(1, 'round');
         break;
       case 2:
-        declareWinner(2, 'round');
         p2Wins++;
+        declareWinner(2, 'round');
         break;
       case 3:
         declareTie();
-        i--;
         break;
     }
-    
+
     if (p1Wins == roundsToWin) {
       declareWinner(1, 'game');
       return;
     } else {
        if (p2Wins == roundsToWin) {
-         declareWinner(2, 'game') ;
+         declareWinner(2, 'game');
          return;
         }
     }
-    
-  }
-}
 
-playGame(5);
+  }
+
+
